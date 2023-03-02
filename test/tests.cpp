f229f7d8c1df036d2679b267332241691d951594
Jelle van Kraaij <jellevankraaij@gmail.com>
2023-03-02 15:25:56 +0100

    fix tests on linux

--- test/tests.cpp
+++ test/tests.cpp
@@ -45,6 +45,14 @@ TEST(XPM42, invalidFile) {
 	free(data);
 }
 
+static const char getline_testfile[] =
+"CJNh7W73d55&%Du\n"
+"a#x@yNCUZ6ZRfAw\n"
+"\n"
+"a2F58%Zz#HA$M6n$m4ygjPjF5KNaRK99ZJWvE@vjnFYB#ieMSR9CFu6*GPfvGnDJx$ha$UFvBFH%7M@yyba89ZPETNR%g3YrmKsGczT7w7W6&5!AQWGGb8mLr6dr#D$ca2F58%Zz#HA$M6n$m4ygjPjF5KNaRK99ZJWvE@vjnFYB#ieMSR9CFu6*GPfvGnDJx$ha$UFvBFH%7M@yyba89ZPETNR%g3YrmKsGczT7w7W6&5!AQWGGb8mLr6dr#D$c"
+;
+
+
 TEST(XPM42_int, getline)
 {
 	ssize_t		len;
@@ -52,17 +60,9 @@ TEST(XPM42_int, getline)
 	len = xpm_getline(NULL, NULL, NULL);
 	EXPECT_EQ(len, -1);
 
-	FILE *fp = fmemopen(NULL, 512, "w+");
 
+	FILE *fp = fmemopen((void *)getline_testfile, strlen(getline_testfile), "r");
 	ASSERT_NE(fp, nullptr);
-
-	fputs("CJNh7W73d55&%Du\n", fp);
-	fputs("a#x@yNCUZ6ZRfAw\n", fp);
-	fputs("\n", fp);
-	fputs("a2F58%Zz#HA$M6n$m4ygjPjF5KNaRK99ZJWvE@vjnFYB#ieMSR9CFu6*GPfvGnDJx$ha$UFvBFH%7M@yyba89ZPETNR%g3YrmKsGczT7w7W6&5!AQWGGb8mLr6dr#D$ca2F58%Zz#HA$M6n$m4ygjPjF5KNaRK99ZJWvE@vjnFYB#ieMSR9CFu6*GPfvGnDJx$ha$UFvBFH%7M@yyba89ZPETNR%g3YrmKsGczT7w7W6&5!AQWGGb8mLr6dr#D$c", fp);
-
-	rewind(fp);
-	ASSERT_FALSE(ferror(fp));
 
 	char *line = NULL;
 	size_t n = 0;
